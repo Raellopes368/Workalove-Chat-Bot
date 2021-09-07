ac19f581d2a0953d2ec8c8651fd7083da2afe99d
Raellopes368 <raellopes368@gmail.com>
2021-09-07 15:52:45 -0300

    Deleta componente ChatBot

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home} />
       </Switch>
     </BrowserRouter>
   );
