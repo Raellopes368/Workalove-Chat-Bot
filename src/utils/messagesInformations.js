42f10772a0f0f614288411faa93bc2eaff702a1c
Raellopes368 <raellopes368@gmail.com>
2021-09-07 21:04:32 -0300

    Estrutura de mensagens padrão

---
+++ src/utils/messagesInformations.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 0,
+    message:
+      'Olá, eu sou o Chatnilson, tudo bem? Para começarmos, preciso saber seu nome.',
+    field: 'name',
+  },
+  {
+    id: 1,
+    message:
+      'Que satisfação, %%name%%. Agora que sei seu nome, qual o estado que você mora?',
+    field: 'uf',
+    dependent: 'name',
+    type: 'text',
+  },
+  {
+    id: 2,
+    message: 'E a sua cidade.',
+    field: 'city',
+    dependent: 'uf',
+    type: 'text',
+  },
+  {
+    id: 3,
+    message:
+      'Legal, agora que sabemos sua cidade e estado. Quando foi que você nasceu?',
+    field: 'birth',
+    dependent: 'city',
+    type: 'date',
+  },
+  {
+    id: 4,
+    message: 'Agora me fala teu e-mail, por gentileza.',
+    field: 'email',
+    dependent: 'birth',
+    type: 'email',
+  },
+  {
+    id: 5,
+    message:
+      'Você finalizou o teste. Fça uma avaliação sobre o processo que realizou até chegar aqui. Nós agradecemos!',
+    field: 'satisfaction',
+    dependent: 'email',
+    type: 'text',
+  },
+];
