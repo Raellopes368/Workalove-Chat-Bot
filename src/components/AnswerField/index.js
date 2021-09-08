de14043270b4b9297e3eadfda88bb883472a1ad2
Raellopes368 <raellopes368@gmail.com>
2021-09-07 21:03:27 -0300

    Adiciona borda de acordo com erro

--- src/components/AnswerField/index.js
+++ src/components/AnswerField/index.js
@@ -4,12 +4,34 @@ import { IoSend } from 'react-icons/io5';
 import PropTypes from 'prop-types';
 import './styles.css';
 
-function AnswerField({ name, type }) {
+function AnswerField({ name, type, handleRelease, errors, touched }) {
+  function keyPress(key) {
+    if (key === 'Enter') handleRelease();
+  }
+
+  function getBorderColor() {
+    return errors[name] && touched[name] ? '#e04a3d' : 'rgb(62, 169, 231)';
+  }
+
   return (
     <div className="answerContainer">
       <div className="row">
-        <Field name={name} type={type} />
-        <IoSend size={30} color="rgb(62, 169, 231)" />
+        <Field
+          name={name}
+          type={type}
+          autoFocus
+          style={{
+            borderWidth: 1,
+            borderColor: getBorderColor(),
+          }}
+          autoComplete="off"
+          onKeyPress={(e) => keyPress(e.key)}
+        />
+        <IoSend
+          size={30}
+          color="rgb(62, 169, 231)"
+          onClick={() => handleRelease()}
+        />
       </div>
       <ErrorMessage className="errorMessage" component="span" name={name} />
     </div>
@@ -19,5 +41,9 @@ function AnswerField({ name, type }) {
 AnswerField.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  touched: PropTypes.shape({}).isRequired,
+  handleRelease: PropTypes.func.isRequired,
+  errors: PropTypes.shape({}).isRequired,
 };
+
 export default AnswerField;
